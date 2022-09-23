71ad144d0972dbe5512600a83daaee6df8bc2861
Casey Trombley <casey@algaecal.com>
2022-09-23 12:29:24 -0400

    feature: scaffolding

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,10 +1,11 @@
 <template>
   <form
-      name="contact-speaker"
+      name="contact"
       method="POST"
       data-netlify="true"
       data-netlify-honeypot="bot-field"
   >
+    <input type="hidden" name="form-name" value="contact" />
     <input
         type="hidden"
         name="form-name"
