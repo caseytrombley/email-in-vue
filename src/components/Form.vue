6e82afa06321371d65cc7605501bcb1cb778a338
Casey Trombley <casey@algaecal.com>
2022-09-23 16:08:35 -0400

    feature: form

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -5,11 +5,7 @@
       data-netlify="true"
       data-netlify-honeypot="bot-field"
   >
-    <input
-        type="hidden"
-        name="form-name"
-        value="ask-question"
-    >
+    <input type="hidden" name="form-name" value="contact" />
     <two-button-modal
         title="Send a message"
         @cancel="$emit('cancel')"
