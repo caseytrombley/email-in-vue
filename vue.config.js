2c7f939bae6271618745056dfb854818c1b9b1ab
Casey Trombley <casey@algaecal.com>
2022-09-23 11:02:03 -0400

    feature: scaffolding

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,26 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const path = require('path')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  configureWebpack: () => {
+    if (process.env.NODE_ENV !== 'production') return;
+    return {
+      plugins: [
+        new PrerenderSPAPlugin(
+            // Absolute path to compiled SPA
+            path.resolve(__dirname, 'dist'),
+            // List of routes to prerender
+            [ '/'],
+            {
+              // options
+            }
+        ),
+      ]
+    }
+  }
+}
